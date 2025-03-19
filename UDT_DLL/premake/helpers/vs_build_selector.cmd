015af3b17d32fbf3cc7c1fe591b2e9dbf4a1c582
Michal Mitter <mittermichal@gmail.com>
2025-03-19 10:51:47 +0100

    echo choice

--- UDT_DLL/premake/helpers/vs_build_selector.cmd
+++ UDT_DLL/premake/helpers/vs_build_selector.cmd
@@ -2,6 +2,7 @@ echo Which target?
 echo 1. Release
 echo 2. Debug
 set /p choice=
+echo %choice%
 if %choice%==1 (
 	set vs_target=Release
 ) else if %choice%==2 (
@@ -15,6 +16,7 @@ echo Which architecture?
 echo 1. x86
 echo 2. x64
 set /p choice=
+echo %choice%
 if %choice%==1 (
 	set vs_arch=Win32
 ) else if %choice%==2 (
